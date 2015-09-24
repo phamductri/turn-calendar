dc71988563e7af88cbf66a872b9aa324a091c245
Piyush Sukhadiya <piyushsu@cybage.com>
2015-09-24 18:46:20 +0530

    Modified to close calendar on mouse up instead of mouse click
    to support firefox
    
    Jira: TRA-1901
    Signed-off-by: Piyush Sukhadiya <piyushsu@cybage.com>

--- dist/turnCalendar.js
+++ dist/turnCalendar.js
@@ -1419,13 +1419,19 @@ angular.module('turn/calendar', ['calendarTemplates']).constant('turnCalendarDef
          * This will make sure that click outside of calendar will close the calendar
          * (behave same as cancel button click)
          */
-    $document.bind('click', function (event) {
-      if (!angular.element('turn-calendar').find(event.target).length) {
-        $scope.$apply(function () {
-          if ($scope.currentSelectedStartDate && $scope.currentSelectedEndDate) {
-            $scope.cancel();
+    $scope.$watch('calendarEnabled', function (isEnabled) {
+      if (isEnabled) {
+        $document.on('mouseup', function (event) {
+          if (!angular.element('turn-calendar').find(event.target).length) {
+            $scope.$apply(function () {
+              if ($scope.currentSelectedStartDate && $scope.currentSelectedEndDate) {
+                $scope.cancel();
+              }
+            });
           }
         });
+      } else {
+        $document.off('mouseup');
       }
     });
     $scope.$watch('timezone', function (newVal) {
